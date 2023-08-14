e359a37b68a250411391295e9096a200bb0edad7
Mbilalshafqat <ua46792@gmail.com>
2023-08-14 22:20:57 +0500

    html oage section

--- client/src/LanguageLearning/MainPage/Index.jsx
+++ client/src/LanguageLearning/MainPage/Index.jsx
@@ -3,14 +3,18 @@ import Sider from "./Sidebar/Sider";
 import Contentbar from "./ContentBar/Contentbar";
 import { useParams } from "react-router-dom";
 import "./Style/Index.css";
+import { sideArray } from "../LanguageStaticData/SidebarData/SidebarStaticData";
 
 const Index = () => {
   const { name } = useParams();
+  const filterSidebar =
+    sideArray && sideArray.filter((item, index) => item.language === name);
+
   return (
     <>
       <div className="language_index">
         <div className="left_part">
-          <Sider />
+          <Sider data={filterSidebar} />
         </div>
         {/* ---------------------  */}
         <div className="right_part">
--- client/src/LanguageLearning/MainPage/Sidebar/Sider.jsx
+++ client/src/LanguageLearning/MainPage/Sidebar/Sider.jsx
@@ -1,8 +1,60 @@
 import React from "react";
 import "../Style/Sidebar.css";
 
-const Sider = () => {
-  return <div className="sidebar_language">Sider</div>;
+const Sider = ({ data }) => {
+  console.log(data);
+
+  return (
+    <div className="sidebar_language">
+      {/* =================== introduction  */}
+      {data &&
+        data.map((item, index) => {
+          return (
+            <div key={index}>
+              <SidebarRiteData title="Introduction" data={item.introduction} />
+            </div>
+          );
+        })}
+      {/* ========================== basic_tag */}
+      {data &&
+        data.map((item, index) => {
+          return (
+            <div key={index}>
+              <SidebarRiteData title="HTML BASIC TAGS" data={item.basic_tag} />
+            </div>
+          );
+        })}
+      {/* ========================== formatingTags */}
+      {data &&
+        data.map((item, index) => {
+          return (
+            <div key={index}>
+              <SidebarRiteData
+                title="HTML FORMATTING TAGS"
+                data={item.formatingTags}
+              />
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
 export default Sider;
+
+// ---------------------- get data
+
+const SidebarRiteData = ({ title, data }) => {
+  return (
+    <div className="sidebar_data_content">
+      <h2>{title}</h2>
+      {data.map((item, index) => {
+        return (
+          <div key={index}>
+            <li>{item}</li>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
