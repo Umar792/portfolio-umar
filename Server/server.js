54f3c250b8c351761ed3531e379142d6c072c2ff
Mbilalshafqat <ua46792@gmail.com>
2023-08-17 17:28:47 +0500

    backend Start

---
+++ Server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+// ------------------- dotenv
+require("dotenv").config();
+
+// -------------- cors
+var cors = require("cors");
+app.use(cors());
+
+// ---------------- body parser
+var bodyParser = require("body-parser");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// ---------------------- creating server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
+
+// ============================== Error Handling
+process.on("uncaughtException", (err) => {
+  console.log(`Unhandled error due to uncaughtException: ${err.message}`);
+  console.log("Server shutting down");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Unhandled error due to unhandledRejection: ${err.message}`);
+  console.log("Server shutting down");
+  server.close(() => {
+    process.exit(1);
+  });
+});
