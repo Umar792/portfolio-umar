3b704f976150804c5f9f5585e985af7b7a7d1dcc
umar793 <106830172+Umar792@users.noreply.github.com>
2023-08-18 22:11:04 +0500

    redux updte

--- client/src/Redux/Action/UserAction.js
+++ client/src/Redux/Action/UserAction.js
@@ -4,17 +4,20 @@ export const UserRegistration =
   (name, email, password, navigate) => async (dispatch) => {
     try {
       dispatch({ type: "loadUserRegistration" });
-      const res = await fetch("http://localhost:4000/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          password,
-        }),
-      });
+      const res = await fetch(
+        "https://sapphire-skunk-coat.cyclic.cloud/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            password,
+          }),
+        }
+      );
       dispatch({ type: "userRegistrationFail" });
       const data = await res.json();
       console.log(data);
@@ -34,7 +37,7 @@ export const UserRegistration =
 export const loginuser = (email, password, navigate) => async (dispatch) => {
   try {
     dispatch({ type: "loadUserLogin" });
-    const res = await fetch("http://localhost:4000/login", {
+    const res = await fetch("https://sapphire-skunk-coat.cyclic.cloud/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -62,13 +65,16 @@ export const loginuser = (email, password, navigate) => async (dispatch) => {
 export const AuthanticateUser = () => async (dispatch) => {
   try {
     dispatch({ type: "loadUserLogin" });
-    const res = await fetch("http://localhost:4000/loginuser", {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-        token: localStorage.getItem("myblogtoken"),
-      },
-    });
+    const res = await fetch(
+      "https://sapphire-skunk-coat.cyclic.cloud/loginuser",
+      {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          token: localStorage.getItem("myblogtoken"),
+        },
+      }
+    );
     dispatch({ type: "LoadUserLoginFail" });
     const data = await res.json();
     if (res.status === 400 || !data) {
@@ -86,19 +92,22 @@ export const CreateBlog =
   (title, category, description, BlogImage) => async (dispatch) => {
     try {
       dispatch({ type: "CreateBlog" });
-      const res = await fetch("http://localhost:4000/createBlog", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          token: localStorage.getItem("myblogtoken"),
-        },
-        body: JSON.stringify({
-          title,
-          category,
-          description,
-          BlogImage,
-        }),
-      });
+      const res = await fetch(
+        "https://sapphire-skunk-coat.cyclic.cloud/createBlog",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            token: localStorage.getItem("myblogtoken"),
+          },
+          body: JSON.stringify({
+            title,
+            category,
+            description,
+            BlogImage,
+          }),
+        }
+      );
       dispatch({ type: "CreateBlogFail" });
       const data = await res.json();
       console.log(data);
@@ -117,12 +126,15 @@ export const CreateBlog =
 export const GetAllBlogs = () => async (dispatch) => {
   try {
     dispatch({ type: "GetAllBlogs" });
-    const res = await fetch("http://localhost:4000/getAllBlogs", {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const res = await fetch(
+      "https://sapphire-skunk-coat.cyclic.cloud/getAllBlogs",
+      {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     dispatch({ type: "GetAllBlogsFail" });
     const data = await res.json();
     if (res.status === 400 || !data) {
@@ -138,12 +150,15 @@ export const GetAllBlogs = () => async (dispatch) => {
 export const GetSingleBlog = (id) => async (dispatch) => {
   try {
     dispatch({ type: "GetSingle" });
-    const res = await fetch(`http://localhost:4000/getSingleBlog/${id}`, {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const res = await fetch(
+      `https://sapphire-skunk-coat.cyclic.cloud/getSingleBlog/${id}`,
+      {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     dispatch({ type: "GetSingleFail" });
     const data = await res.json();
     if (res.status === 400 || !data) {
